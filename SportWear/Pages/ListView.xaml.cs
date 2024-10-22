df4373edd00ac0ae8514b3f00591c004a1e9938d
Коммерческие курсы <comcuru@teach.local>
2024-10-22 11:32:18 +0300

    22.10

---
+++ SportWear/Pages/ListView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SportWear.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ListView.xaml
+    /// </summary>
+    public partial class ListView : Page
+    {
+        public ListView()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.Manager.MainFrame.Navigate(new Pages.Auth());
+            return;
+
+        }
+
+        private void AddUserButton_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.Manager.MainFrame.Navigate(new Pages.UserAdd());
+            return;
+        }
+    }
+}
